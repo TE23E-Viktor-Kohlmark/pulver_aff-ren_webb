19656f99dbc25e6be3220a70185406a69cc7079e
kohlmark.viktor <viktor.kohlmark.ntig.se>
2024-12-05 14:25:23 +0100

    Basket, local storage, increment, decrement, upadet, showing

--- main.js
+++ main.js
@@ -15,10 +15,12 @@ let shopItemsData = [
     },
 ];
 
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
-
-let generateShop = () => {
-    return (shop.innerHTML = shopItemsData.map((x) => {
+// Make in to a function insted of a var 
+function generateShop() {
+    shop.innerHTML = shopItemsData.map((x) => {
+        let search = basket.find((x) => x.id === x.id) || [];
         return `
             <div class="item">
                 <div class="details">
@@ -26,14 +28,49 @@ let generateShop = () => {
                     <div class="text">
                         <h3>${x.name}</h3>
                         <div class="price-buy">
-                            <button>Köp</button>
+                            <button onclick="increment('${x.id}')"> Köp</button>
                             <p>Pris : ${x.price}</p>
                         </div>                    
                     </div> 
                 </div>
             </div>
-        `
-    }).join(""));
-};
+        `;
+    }).join("");
+}
 
 generateShop();
+
+let increment = (id) => {
+    let selectedItem = shopItemsData.find((x) => x.id === id);
+
+    if (selectedItem) {
+        let search = basket.find((x) => x.id === selectedItem.id);
+        if (search === undefined) {
+            basket.push({
+                id: selectedItem.id,
+                item: 1,
+            });
+        } else {
+            search.item++;
+        }
+        localStorage.setItem("data", JSON.stringify(basket));
+        console.log(basket);
+        update(selectedItem.id);
+    } 
+};
+
+let dncrement = () => { };
+let update = (id) => { 
+    let search = basket.find((x) => x.id === id);
+    console.log(search.item);
+    console.log("update is running");
+    calculation();
+};
+
+let calculation = () => {
+    let carticon = document.getElementById('cartAmount');
+    carticon = document.getElementById('cartAmount');
+    console.log(basket.map((x) => x.item).reduce((x, y) => x + y, 0));
+};
+
+calculation();
